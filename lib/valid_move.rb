7730f55e5f240d28c40a0bf88ba6527dd409a23b
Manuela Redinciuc <manutzu15@gmail.com>
2020-02-01 12:44:41 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,13 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-
-
+if index.between?(0,8) && position_taken? == true
+return true
+else
+  return false
+end
 end
 
+#Not already filled with a token.
 
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
